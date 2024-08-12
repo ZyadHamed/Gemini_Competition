d62265ce59472f0f0e8e40513dccbc5026b968b8
ZyadHamed <zyadhamedashour@gmail.com>
2024-08-12 20:26:59 +0300

    Some changes

--- Gemeni Competition/Program.cs
+++ Gemeni Competition/Program.cs
@@ -1,9 +1,18 @@
 using Gemini_Competition.Services;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 //Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", @"firebasecredientials.json");
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    // Get the port from environment variables, default to 5000 if not set
+    var port = Convert.ToInt32(Environment.GetEnvironmentVariable("PORT") ?? "5000");
+
+    // Listen on all available IP addresses
+    serverOptions.Listen(IPAddress.Any, port);
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -18,6 +27,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
 app.UseHttpsRedirection();
 
